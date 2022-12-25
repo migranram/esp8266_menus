d5c8ac2766139dc138cd99a9888533415d9ab9bf
miguel <miguelgranero99@gmail.com>
2022-12-25 11:12:21 +0100

    license header added

--- include/pages.hpp
+++ include/pages.hpp
@@ -1,5 +1,36 @@
 #include "menu.hpp"
 
+/*
+
+This software uses part of the TFT_eSPI library examples source code (TFT_Terminal).
+Below its BSD License.
+
+*/
+
+/*************************************************************
+  This sketch implements a simple serial receive terminal
+  program for monitoring serial debug messages from another
+  board.
+  
+  Connect GND to target board GND
+  Connect RX line to TX line of target board
+  Make sure the target and terminal have the same baud rate
+  and serial stettings!
+
+  The sketch works with the ILI9341 TFT 240x320 display and
+  the called up libraries.
+  
+  The sketch uses the hardware scrolling feature of the
+  display. Modification of this sketch may lead to problems
+  unless the ILI9341 data sheet has been understood!
+
+  Updated by Bodmer 21/12/16 for TFT_eSPI library:
+  https://github.com/Bodmer/TFT_eSPI
+  
+  BSD license applies, all text above must be included in any
+  redistribution
+ *************************************************************/
+
 // -----------------------------------------------
 /// @brief Page with title and content
 class TitledContentPage : public Page
